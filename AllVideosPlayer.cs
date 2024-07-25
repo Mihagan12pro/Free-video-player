94f41e4077721aa8a20c9172e86e1cb316a1f0d8
Mihagan12pro <mikhail.kargin.05@mail.ru>
2024-07-25 10:56:36 +0300

    The AllVideosPlayer class was created.

---
+++ AllVideosPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Free_video_player
+{
+    internal class AllVideosPlayer : AbstractPlayer
+    {
+        protected override void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PauseAndPlay_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SkipAndForward_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void VideoSlider_Change(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void VolumeSlider_Change(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
